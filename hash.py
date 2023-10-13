6c34b93d91d68d4f14f79c58e1b8735621f5e806
Gvelecico <gvelecico@hotmail.com>
2023-10-12 21:30:04 -0300

    codigo final

--- hash.py
+++ hash.py
@@ -2,17 +2,40 @@ import hashlib
 
 
 
-myText = None
+
 
 def menu():
-    myText = input("Insira a frase que o hash precisa ser validada: ")
+    global myText 
+    global SHA256_Validation
+    global MD5_Validation
+    myText = input(str("Insira a frase que o hash precisa ser validada: \n" ))
+    SHA256_Validation =  input(str("Insira a hash no padrão SHA256 para ser validada \n"))
+    MD5_Validation =  input(str("Insira a hash no padrão MD5 para ser validada \n"))
+
+
+
+
+def hash(myText, SHA256_Validation, MD5_Validation):
     SHA256 = hashlib.sha256(myText.encode('utf-8')).hexdigest()
     MD5 = hashlib.md5(myText.encode('utf-8')).hexdigest()
-    print("\"" +myText + "\" - " + SHA256 + " - " + MD5)
 
-
-
-
+    print("\"" + myText + "\" - " + SHA256 + " - " + MD5 + "\n")
     
-
-menu()
+    if SHA256 == SHA256_Validation and MD5 == MD5_Validation:
+        print("Hash correto em ambos padrões\n")
+    elif SHA256 == SHA256_Validation:
+        print("Hash correto no padrão SHA256\n")
+    elif MD5 == MD5_Validation:
+        print("Hash correto no padrão MD5\n")
+    else:
+        print("Nenhuma validação correta\n")
+            
+        
+loop = 1
+    
+while loop == 1:
+    menu()
+    hash(myText, SHA256_Validation, MD5_Validation)
+    choice = input("Para testar outra frase, digite 1. Para sair, digite 0: ")   
+    if choice != '1':
+        loop = 0
